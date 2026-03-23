0e83a114d55d999934c86686c4ad68450c2b5945
cronus <bocui552@gmail.com>
2026-03-23 09:07:33 +0800

    update topological_sort to check is_constant

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -73,13 +73,16 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     """
     # TODO: Implement for Task 1.4.
     #raise NotImplementedError("Need to implement for Task 1.4")
+    #print("enter topo sort")
     visited_node = [variable]
     #print(variable)
-    #print("is_leaf")
+    #print("is_leaf:")
     #print(variable.is_leaf())
-    if not variable.is_leaf():
-        for parent in variable.parents:
-            visited_node += topological_sort(parent)
+    #print(variable.parents)
+    if not variable.is_constant():
+        if not variable.is_leaf():
+            for parent in variable.parents:
+                visited_node += topological_sort(parent)
     #print("after topo sort:")
     #print(visited_node)
     return visited_node
@@ -102,16 +105,17 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
     variable.derivative = deriv
     for sorted_var in topological_sort(variable):
         #print(sorted_var)
-        if (not sorted_var.is_leaf()):
-            for (var, local_deriv) in sorted_var.chain_rule(sorted_var.derivative):
-                #print("var:" + str(var.unique_id))
-                #print(var)
-                #print("local_deriv:")
-                #print(local_deriv)
-                if (var.is_leaf()):
-                    var.accumulate_derivative(local_deriv)
-                else:
-                    var.derivative = local_deriv
+        if not sorted_var.is_constant():
+            if (not sorted_var.is_leaf()):
+                for (var, local_deriv) in sorted_var.chain_rule(sorted_var.derivative):
+                    #print("var:" + str(var.unique_id))
+                    #print(var)
+                    #print("local_deriv:")
+                    #print(local_deriv)
+                    if (var.is_leaf()):
+                        var.accumulate_derivative(local_deriv)
+                    else:
+                        var.derivative = local_deriv
             
 
 
